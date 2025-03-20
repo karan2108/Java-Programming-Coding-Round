09b6b6a50c14927f878bc5ebb195dab33577de49
karan2108 <77577426+karan2108@users.noreply.github.com>
2025-03-20 11:22:06 +0530

    Emergency Response Systems

--- EmergencyResponseSystem.java
+++ EmergencyResponseSystem.java
@@ -40,7 +40,7 @@
 
 import java.util.*;
 
-public class Main {
+public class EmergencyResponseSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
